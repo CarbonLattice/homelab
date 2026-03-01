fe4ed4c7303e936130d2631bf662700b65044c32
simon <carboncreationssp3@gmail.com>
2026-02-28 17:35:40 -0800

    first commit for the new setup with 3 nodes

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "carbon-lab"
 
     workspaces {
       name = "homelab-external"
